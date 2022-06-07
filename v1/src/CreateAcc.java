96f64984f6f6edf9ca5a7652b228fe4e76b7c028
kelvinxu1031 <106162687+kelvinxu1031@users.noreply.github.com>
2022-06-07 19:14:12 -0400

    Update CreateAcc.java

--- v1/src/CreateAcc.java
+++ v1/src/CreateAcc.java
@@ -2,9 +2,14 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.io.*;
-public class CreateAcc extends JFrame implements ActionListener{
-	private static JFrame createAccF;
 
+/**
+ * This program allows for the user to create an account to access the game
+ * Author: Kelvin Xu
+ * Date: June 7, 2022
+ */
+public class CreateAcc extends JFrame implements ActionListener{
+	
 	//file IO variables
 	private static String file = "accounts.txt";
 	private static String[][] accounts = new String[2][1000];
@@ -12,62 +17,116 @@ public class CreateAcc extends JFrame implements ActionListener{
 	private String[] passwords;
 	private static int numOfUsers;
 
-	private JPanel loginInfo = new JPanel(new GridLayout(2,2));
-	private JPanel buttons = new JPanel(new GridLayout(1,2));
-	private JLabel username = new JLabel("USERNAME");
-	private JLabel password = new JLabel("PASSWORD");
-	private JTextField uText = new JTextField();
-	private JPasswordField pText = new JPasswordField();
-	private JButton enter = new JButton("ENTER");
-	private JButton back = new JButton("BACK");
+	private static JFrame  createAccF;
+	private JPanel         backgroundP;
+	private JLabel         lblTitle;
+	private JLabel         lblUser;
+	private JLabel         lblPass;
+	private JTextField     uText;
+	private JPasswordField pText;
+	private JButton        enter;
+	private JButton        back;
 
-
-	public CreateAcc(String title) throws IOException {
+	String titleFontName = "fonts/titleFont.ttf";
+	String textFontName  = "fonts/textFont.ttf";
+	
+	public CreateAcc(String title) throws Exception {
+		//import fonts
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		Font textFont = Font.createFont(Font.TRUETYPE_FONT, new File(textFontName)).deriveFont(12f);
+		ge.registerFont(textFont);
+		Font titleFont = Font.createFont(Font.TRUETYPE_FONT, new File(titleFontName)).deriveFont(30f);
+		ge.registerFont(titleFont);
+		//read input from "accounts.txt"
 		BufferedReader in = new BufferedReader(new FileReader(file));
 		usernames = in.readLine().split(" ");
 		passwords = in.readLine().split(" ");
 		numOfUsers = usernames.length;
-		for(int i = 0; i<usernames.length;i++) {
+		for (int i = 0; i<numOfUsers;i++) {
 			accounts[0][i] = usernames[i];
 			accounts[1][i] = passwords[i];
 		}
 
+		//instantiating components for GUI
+		createAccF  = new JFrame(title);
+		backgroundP = new JPanel();
+		lblTitle    = new JLabel("CREATE YOUR ACCOUNT");
+		lblUser     = new JLabel("USERNAME: ");
+		lblPass     = new JLabel("PASSWORD: ");
+		uText       = new JTextField();
+		pText       = new JPasswordField();
+		enter       = new JButton("ENTER");
+		back        = new JButton("BACK");
 
-		createAccF = new JFrame(title);
-		createAccF.setLayout(new BorderLayout());
-		loginInfo.add(username);
-		loginInfo.add(uText);
-		loginInfo.add(password);
-		loginInfo.add(pText);
-		back.addActionListener(this);
-		enter.addActionListener(this);
-		buttons.add(back);
-		buttons.add(enter);
-		createAccF.add(loginInfo, BorderLayout.CENTER);
-		createAccF.add(buttons, BorderLayout.SOUTH);
-		createAccF.setSize(640,480);
+		//formatting of the components
+		createAccF.setLayout(null);
+		backgroundP.setLayout(null);
+		backgroundP.setSize(720,450);
+		lblTitle.setFont(titleFont);
+		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitle.setBounds(50, 50, 620, 50);
+		lblUser.setFont(textFont);
+		lblUser.setBounds(50,150, 150,25);
+		lblPass.setFont(textFont);
+		lblPass.setBounds(50,250,150,25);
+		uText.setBounds(200,150,470,25);
+		pText.setBounds(200,250,470,25);
+		enter.setBounds(30,325,320,50);
+		back.setBounds(370,325,320,50);
+		
+		//add functionality to buttons
+		createButton(enter);
+		createButton(back);
+
+		backgroundP.add(lblTitle);
+		backgroundP.add(lblUser);
+		backgroundP.add(lblPass);
+		backgroundP.add(uText);
+		backgroundP.add(pText);
+		backgroundP.add(enter);
+		backgroundP.add(back);
+		createAccF.add(backgroundP);
+		createAccF.setSize(720, 450);
 		createAccF.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		createAccF.setLocationRelativeTo(null);
 		createAccF.setVisible(true);
 		in.close();
 	}
 
-	public void createUser(String username, String password){
+
+	/**
+	 * This method takes a button and adds an action listener 
+	 * as well as sets the font for the button
+	 * @param b A button
+	 * @throws Exception font exception
+	 */
+	public void createButton(JButton b) throws Exception{
+		//import fonts
+		Font font = Font.createFont(Font.TRUETYPE_FONT, new File(titleFontName)).deriveFont(12f);
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		ge.registerFont(font);
+		b.addActionListener(this);
+		b.setFont(font);
+	}
+
+	/**
+	 * This method adds a user to the accounts array and updates the text file
+	 * @param username of the user
+	 * @param password of the user
+	 * @throws Exception IOexception
+	 */
+	public void createUser(String username, String password) throws Exception{
 		accounts[0][numOfUsers] = username;
 		accounts[1][numOfUsers] = password;
 		numOfUsers++;
+		saveUsers();
 	}
-
-	public boolean isRegistered(String username) throws IOException{
-		for(int i = 0; i<numOfUsers;i++) {
-			if (accounts[0][i].equals(username)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public static void saveUsers() throws IOException{
+	
+	/**
+	 * This method updates the accounts.txt file 
+	 * @throws Exception
+	 */
+	public void saveUsers() throws Exception{
 		BufferedWriter out = new BufferedWriter(new FileWriter(file));
 		for(int i = 0; i<numOfUsers;i++) {
 			out.write(accounts[0][i]);
@@ -78,34 +137,59 @@ public class CreateAcc extends JFrame implements ActionListener{
 			out.write(accounts[1][i]);
 			out.write(" ");
 		}
-		out.close();
+		out.close();//save .txt file
 	}
 
+	/**
+	 * This method returns whether an account has been registered with this username
+	 * @param username entered by the user
+	 * @return whether the username is already registered
+	 * @throws Exception IOException
+	 */
+	public boolean isRegistered(String username) throws Exception{
+		for(int i = 0; i<numOfUsers;i++) {
+			if (accounts[0][i].equals(username)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Method that detects actions by the user
+	 */
+	@SuppressWarnings("deprecation")
 	public void actionPerformed(ActionEvent e){
-		if (e.getSource() == enter) {
-			String user = uText.getText();
-			String pass = pText.getText();
-			try {
-				if(isRegistered(user)) {
+		try {
+			if (e.getSource() == enter) {//if user clicked enter button
+				
+				//get username and password entered
+				String user = uText.getText();
+				String pass = pText.getText();
+
+				if (pass.length()==0||user.length()==0) {
+					JOptionPane.showMessageDialog(this, "Error: Fill out both username and password fields.");
+				}
+				else if(isRegistered(user)) {
 					uText.setText("");
 					pText.setText("");
 					JOptionPane.showMessageDialog(this, "Error: An account with this username already exists.");
-
 				}
 				else {
 					createUser(user, pass);
-					CreateAcc.saveUsers();
 					createAccF.dispose();
 					Login loginF = new Login("LOGIN");
 				}
 
-				if(e.getSource() == back) {
-					createAccF.dispose();
-					Login loginF = new Login("LOGIN");
-				} 
-			}catch (Exception e1) {
-				System.out.println("Error with file IO");
+
+
 			}
+			if(e.getSource() == back) {//if user clicked back button
+				createAccF.dispose();
+				Login loginF = new Login("LOGIN");
+			} 
+		}catch (Exception e1) {
+			System.out.println("Error with file IO");
 		}
 	}
 
